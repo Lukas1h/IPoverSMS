c2b625dd65c33585b899d34f9d2ee38913dca2a0
Lukas1h <Lukas1h7@gmail.com>
2022-09-23 14:08:44 -0700

    Added stuf

--- index.js
+++ index.js
@@ -2,21 +2,26 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const request = require('request');
+const requestNull = require('request').defaults({ encoding: null });
+const fs = require('fs');
+
+const jsdom = require("jsdom")
+const { JSDOM } = jsdom
+global.DOMParser = new JSDOM().window.DOMParser
 
 
-var inbox = require("inbox");
-const fetch = require('node-fetch')
+const inbox = require("inbox");
 const nodemailer = require("nodemailer");
 
 
 //Swn7448810isthebestpassword
-let transporter = nodemailer.createTransport({
-    host: "mail.smtp2go.com",
-    port: 2525,
-    secure: false, // true for 465, false for other ports
-    auth: {
-        user: "lukas1h", // generated ethereal user
-        pass: "Swn7448810", // generated ethereal password
+
+
+const transporter = nodemailer.createTransport({
+  service: 'gmail',
+  auth: {
+        user: "lukas1h07@gmail.com", // generated ethereal user
+        pass: "pbxvayjrvbryiiul", // generated ethereal password
     },
 });
 
@@ -46,14 +51,6 @@ client.on("connect", function(){
                     result += chunk;
                 });
                 readable.on('end', function () {
-                    /*
-                        Example message:
-                        {
-                            "url":"https://www.reddit.com/r/cgiMemes/top/?t=all",
-                            "type":"img"
-                        }
-                        */
-                    
                     
                     var msg = parseMessage(result).substring(10)
                         
@@ -70,25 +67,83 @@ client.on("connect", function(){
                     console.log("\nURL:"+url+"\ntype:"+type+"\n")
 
                     console.log("Checking if URL is valid.\n")
+
                     if(isValidUrl(url) || true){ //Its the truth, aint it?
                         console.log("Valid.\n")
                         if(type=="file"){
                             console.log("Sending FILE Message...\n")
-                            
+                            console.log("Fetching "+url+"\n")
+                        
+                            request(url, function (error, response, body) {
+                                if (!error) {
+                                    console.log("Done. "+"\n")
+                                    console.log("Got HTML"+body.substring(0,10))
+                                    let html = body
+                                    console.log("Parsing HTML "+"\n")
+                                    let DOMParsing = new DOMParser()
+                                    let parsed = DOMParsing.parseFromString(body, "text/html")
+                                    console.log("Done. "+"\n")
+                                    console.log("Getting images. "+"\n")
+                                    let images = Array.prototype.map.call(parsed.images, img => img.src)
+                                    console.log("Done. " + images[0] + "\n")
+                                    
+
+                                    let i = 0
+                                    images.forEach(image => {
+                                        let imageUrl
+                                        if(image.startsWith("//")){
+                                            imageUrl = "https:"+image
+                                            console.log("Reddit-style URL. Fixing. " + imageUrl.substring(0,20)+ "\n")
+                                        }else if(image.startsWith("/")){
+                                            imageUrl = "https://" + getHostname(url) + image
+                                            console.log("Relitive URL. Fixing. " + imageUrl.substring(0,20)+ "\n")
+                                        }else{
+                                            imageUrl = image
+                                            console.log("Standerd URL. " + imageUrl.substring(0,20)+ "\n")
+                                        }
 
 
-                            transporter.sendMail({
-                                from: 'lukas1h07@gmail.com',
-                                to: "5414303372@mms.uscc.net ",
-                                subject: "__",
-                                text: "__",
-                                html: "__",
-                                attachments:[{   // encoded string as an attachment
-                                    filename: 'cat.html',
-                                    path:url
-                                }]
+                                        console.log("Downloading Image.\n")
+                                        requestNull.get(imageUrl, function (error, response, body) {
+                                            if (!error && response.statusCode == 200) {
+                                                console.log("Done.\n")
+                                                console.log("Converting to base64.\n")
+                                                data = "data:" + response.headers["content-type"] + ";base64," + Buffer.from(body).toString('base64');
+                                                console.log("Done.\n")
+                                                console.log("Replaceing: ",image.substring(0,20))
+                                                console.log("With:",data.substring(0, 20) ,"\n")
+                                                html = html.replaceAll(image,data)
+                                                i++
+                                                if(i==images.length){
+                                                    console.log("Done patching. Sending.")
+                                                    fs.writeFileSync("./out.html",html)
+                                                    transporter.sendMail({
+                                                        from: 'lukas1h07@gmail.com',
+                                                        to: "5414303372@mms.uscc.net",
+                                                        subject: "__",
+                                                        text: "__",
+                                                        html: "__",
+                                                        attachments:[{   // encoded string as an attachment
+                                                            filename: 'cat.html',
+                                                            content: html,
+                                                            contentType: 'text/html'
+                                                        }]
+                                                    });
+                                                    console.log("Done.")
+
+
+                                                }
+                                            }else{
+                                                console.log("Error downloading image. " + response)
+                                            }
+                                        });
+                                    });
+                                } else {
+                                    console.log(error);
+                                }
                             });
-                            console.log("Done.")
+
+                            
                             
                         }else{
                             console.log("Sending SCREENSHOT Message...\n")
@@ -121,19 +176,18 @@ client.on("connect", function(){
 });
 
 
-function removeItem(array, item) {
-    var i = array.length;
-
-    while (i--) {
-        if (array[i] === item) {
-            array.splice(array.indexOf(item), 1);
-        }
-    }
-}
 
 function getPosition(string, subString, index) {
   return string.split(subString, index).join(subString).length;
 }
+
+const getHostname = (url) => {
+  // run against regex
+  const matches = url.match(/^https?\:\/\/([^\/?#]+)(?:[\/?#]|$)/i);
+  // extract hostname (will be null if no match is found)
+  return matches && matches[1];
+}
+
 
 function parseMessage(str){
     boundry=str.match(/^\sboundary.*\n?/gm, '')[0].match(/"(.*?)"/)[1];
@@ -165,6 +219,7 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`)
 })
+
 
 
 // console.log("Fetching "+"https://www..com/search.json?q="+encodeURIComponent(msg))
